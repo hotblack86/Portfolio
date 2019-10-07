4501e771eb6926c6e68914d80ca36271b293073b
James Hotblack <james.c.hotblack@gmail.com>
2019-10-07 19:47:17 +0100

    User can now search my github repos by name or keyword.

--- src/components/githubAPI.js
+++ src/components/githubAPI.js
@@ -4,25 +4,54 @@ import Repo from './repo'
 const GithubAPI = () => {
   
   const [repos, setRepos] = useState([]);
-  // const [search, setSearch] = useState("");
-  // const [query, setQuery] = useState("");
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
 
   useEffect( () => {
     getRepos();
-  });
+  }, [query]);
 
 
   const getRepos = async () => {
-    // let url
-    // (query !== "") ? url = `&beer_name=${query}` : url = "";
-    const response = await fetch(`https://api.github.com/users/hotblack86/repos`);
+    let url
+    if (query !== "") { 
+      url = `https://api.github.com/search/repositories?q=${query}+in:name+user:hotblack86`
+    } else { 
+      url = "https://api.github.com/users/hotblack86/repos?per_page=100";
+    }
+
+    const response = await fetch(url);
     const data = await response.json();
-    console.log(data)
-    setRepos(data);
+    (query !== "") ? setRepos(data.items) : setRepos(data);
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
   }
 
   return (
     <div className="githubAPI">
+
+      <form onSubmit={getSearch} className="search-form">
+       <input 
+          className="search-bar" 
+          type="text" 
+          value={search} 
+          onChange={updateSearch}
+       />
+       <button className="title" type="submit">
+         Search
+       </button>
+      </form>
+
+      <p className="title">Showing {repos.length} results for "{query}"</p>
+
       {repos.map(repo => (
        <Repo 
        key={repo.id}
--- src/components/repo.js
+++ src/components/repo.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Repo = ({title, language, created_at, url}) => {
+
+  
+
   return(
     <div >
       <a 
@@ -8,12 +11,13 @@ const Repo = ({title, language, created_at, url}) => {
         target="_blank" 
         rel="noopener noreferrer"
         style={{
-          color: `black`,
+          color: `green`,
           textDecoration: 'none',
         }}
         >{title}</a>
       <p>Language: {language}</p>
       <p>Created on: {created_at}</p>
+      ~~~
     </div>
   )
 }
